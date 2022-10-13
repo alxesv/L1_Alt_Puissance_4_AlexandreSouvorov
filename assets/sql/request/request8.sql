88fc17911f1b52c568ac79867d52e866c3522f1a
admin <admin@MacBook-Pro-de-admin-7.local>
2022-10-13 16:07:25 +0200

    requetes sql

--- assets/sql/request/request8.sql
+++ assets/sql/request/request8.sql
@@ -1,6 +1,6 @@
 -- envoi de message sur le chat
 
-INSERT INTO `message` (`game_id`, `author_id`, `message` , `last_message_time`) VALUES 
+INSERT INTO `message` (`game_id`, `author_id`, `message` , `message_time`) VALUES 
 ( '1', '1', 'je suis meilleur que toi !' , CURRENT_TIMESTAMP ),
 ( '1' , '2', 'tes nul', CURRENT_TIMESTAMP ),
 ( '1', '3', 'bien joué !' , CURRENT_TIMESTAMP ) ;
