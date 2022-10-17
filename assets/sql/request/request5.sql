ba68b39eea119988d4eebb2aca898b307f00aadd
admin <admin@MacBook-Pro-de-admin-5.local>
2022-10-17 10:52:12 +0200

    footerphp

--- assets/sql/request/request5.sql
+++ assets/sql/request/request5.sql
@@ -1,6 +1,7 @@
 -- Requête permettant d'afficher les scores
 
-SELECT game.game_name as "Jeu", user.pseudo as "Joueur", score.game_difficulty as "Difficulté", score.game_score as "Score" FROM score
+SELECT commentaire.commentaire as "Lescommentaires", user.pseudo as "Joueur", score.game_difficulty as "Difficulté", score.game_score as "Score" FROM score
 INNER JOIN user ON score.player_id = user.user_id
 INNER JOIN game ON score.game_id = game.game_id
 ORDER BY game.game_name ASC, score.game_difficulty, score.game_score;
+
---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/section.css">
+    <link rel="icon" href="assets/images/favicon.jpg"> 
+    <script src="https://kit.fontawesome.com/583bdb9b50.js" crossorigin="anonymous"></script>
+    <title>The Power Of Memory</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <div class="banniere">
+    <header id="main-header">
+        <h1 class="titreacceuil">The Power Of Memory</h1>
+        <nav>
+            <div id="mySidenav" class="sidenav">
+                    <ul class="menunav">
+                        <li><a href="index.html">ACCUEIL</a></li>
+                        <li><a href="memory.html">JEU</a></li>
+                        <li><a href="scores.html">SCORES</a></li>
+                        <li><a href="contact.html">NOUS CONTACTER</a></li>
+                        <li><a href="login.html">SE CONNECTER</a></li>
+                        <li><a href="inscription.html">S'INSCRIRE</a></li>
+                    </ul>
+            </div>
+            <a href="#" id="openBtn">
+                <span class="burger-icon">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </a>
+        </nav>
+    </header>
+     <section class="titreheader">
+        <h2 class="titredebannieres">BIENVENUE DANS NOTRE STUDIOS ! </h2>
+        <h3 class="soustitre">Venez challenger les cerveaux les plus agiles !</h3>
+        <a href="memory.html"><button class="bouttonjouer"> Jouer !</button></a>
+    </section></div> 
+
+
+    <!-- HEADER -->
+
+    <!-- MAIN -->
+    <main>
+
+
+        <!-- 3 cartes -->
+        <section class="card_section">
+            <div class="cards">
+                <img src="http://placekitten.com/g/470/300" alt="">
+                <img src="http://placekitten.com/g/200/300" alt="">
+                <img src="http://placekitten.com/g/120/300" alt="">
+            </div>
+            <div class="card-text">
+                <p>01</p>
+                <div>
+                    <h3>Anime</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam nam deserunt eos pariatur soluta itaque ipsa minima, facere saepe atque officia ducimus error optio enim voluptatum aspernatur odio voluptatem voluptatibus, et modi. Dolor neque nemo nam nesciunt alias officia fugit!</p>
+                </div>
+                <p>02</p>
+                <div>
+                    <h3>Dragon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam exercitationem tempore, officiis asperiores deserunt corrupti magnam voluptatibus, eligendi, in tempora impedit quibusdam maiores dolorum quod suscipit itaque libero distinctio.</p>
+                </div>
+                <p>03</p>
+                <div>
+                    <h3>People</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolore consequatur quidem nobis, necessitatibus repudiandae asperiores iusto maiores et ex at eos ipsum esse tempora harum officiis laboriosam cum vel?</p>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Statistiques -->
+        <section class="stats_section">
+            <img src="http://placekitten.com/g/450/300" alt="">
+            <div class="stats">
+                <div class="row_1">
+                    <div id="games_played" class="stat_box">
+                        <p>500</p> <span>Parties jouées</span> 
+                    </div>
+                    <div id="best_time" class="stat_box">
+                        <p>4s</p> <span>Temps record<nspa > 
+                    </div>
+                </div>
+                <div class="row_2">
+                    <div id="players_online" class="stat_box">
+                        <p>32235</p> <span>Joueurs connectés</span> 
+                    </div>
+                    <div id="registered_players" class="stat_box">
+                        <p>53653</p> <span>Joueurs enregistrés</span> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Notre équipe -->
+        <section class="team_section">
+            <h2>Notre équipe</h2>
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <img src="assets/images/text-separator.png" alt="">
+            <div class="team_members_box">
+                <div class="member">
+                    <img src="assets/images/shikamaru.png" alt="">
+                    <p>Alexandre</p>
+                    <p>Développeur</p>
+                    <p><i class="fa-brands fa-square-facebook"></i> <i class="fa-brands fa-square-twitter"></i> <i
+                            class="fa-brands fa-square-pinterest"></i></p>
+                </div>
+                <div class="member">
+                    <img src="assets/images/leorio.jpg" alt="">
+                    <p>Baudouin</p>
+                    <p>Développeur</p>
+                    <p><i class="fa-brands fa-square-facebook"></i> <i class="fa-brands fa-square-twitter"></i> <i
+                            class="fa-brands fa-square-pinterest"></i></p>
+                </div>
+                <div class="member">
+                    <img src="assets/images/shanks.jpg" alt="">
+                    <p>Djamel</p>
+                    <p>Développeur</p>
+                    <p><i class="fa-brands fa-square-facebook"></i> <i class="fa-brands fa-square-twitter"></i> <i
+                            class="fa-brands fa-square-pinterest"></i></p>
+                </div>
+                <div class="member">
+                    <img src="assets/images/goku.jpg" alt="">
+                    <p>Sofiane</p>
+                    <p>Développeur</p>
+                    <p><i class="fa-brands fa-square-facebook"></i> <i class="fa-brands fa-square-twitter"></i> <i
+                            class="fa-brands fa-square-pinterest"></i></p>
+                </div>
+            </div>
+
+        </section>
+
+
+    </main>
+    <!-- MAIN -->
+
+    
+
+
+
+    <!-- FOOTER -->
+    <?php include 'view/footer.inc.php'
+    ?>
+    <!-- Burger js -->
+    <script>
+        var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.toggle("active");
+}
+
+    </script>
+</body>
+
+</html>
